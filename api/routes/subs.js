7d1f5317cc530b9a5ae9be164fb88c552f7e5d6e
Nikita Dunaev <priest9494@yandex.ru>
2020-04-07 15:39:45 +0700

    Updated extend sub functon

--- api/routes/subs.js
+++ api/routes/subs.js
@@ -8,6 +8,40 @@ const dateFormat = require('dateformat');
 // Validation module (check id exists)
 const valid = require("../../validation/subsValidation");
 
+// Extend sub route
+router.post("/extend", async(req, res) => {
+    // Get info about sub type
+    let typeQuery = await database
+    .select('type_id', 'left_to_pay')
+    .from('subs')
+    .where({
+        id: req.body.id
+    })
+    .first()
+    console.log(req.body)
+    console.log(typeQuery.type_id)
+
+    let typeAdditionalInfo = await database
+    .select()
+    .from('types')
+    .where({
+        id: typeQuery.type_id
+    })
+    .first()
+
+    await database('subs').
+    update({
+        begin_date: req.body.beg_date,
+        end_date: req.body.end_date,
+        left_to_pay: typeAdditionalInfo.cost + typeQuery.left_to_pay,
+        training_left: typeAdditionalInfo.training
+    })
+    .where({
+        id: req.body.id
+    })
+
+    res.sendStatus(200)
+})
 // Check sub number uniq route
 router.post("/checkUniq", async(req, res) => {
     let field = await database('subs')
@@ -24,12 +58,12 @@ router.post("/checkUniq", async(req, res) => {
 // Get 10 latest subs route (default search).
 router.get("/getLatest", async (req, res) => {
     let query = await database
-        .select('subs.id as sub_id', 'sub_number', 'start_time', 'subs.note', 'begin_date', 'end_date', 'training_left', 'left_to_pay', 'clients.fio as client_fio', 'phone_number', 'title', 'training', 'cost', 'trainers.fio as trainer_fio')
+        .select('subs.id as sub_id', 'sub_number', 'start_time', 'subs.note', 'begin_date', 'end_date', 'training_left', 'left_to_pay', 'clients.fio as client_fio', 'clients.id as client_id', 'phone_number', 'title', 'training', 'cost', 'types.id as type_id', 'trainers.id as trainer_id', 'trainers.fio as trainer_fio')
         .from('subs')
         .join('clients', 'subs.client_id', 'clients.id')
         .join('types', 'subs.type_id', 'types.id')
         .join('trainers', 'subs.trainer_id', 'trainers.id')
-        .limit(10)
+        .limit(40)
         
     res.send(query);
 });
@@ -90,13 +124,32 @@ router.post("/edit", async (req, res) => {
         return;
     }
 
+    console.log(req.body)
+    // Get info about old sub type
+    let oldTypeAdditionalInfo = await database
+    .select()
+    .from('types')
+    .where({
+        id: req.body.old_type
+    });
+
+    // Get info about new sub type
+    let newTypeAdditionalInfo = await database
+    .select()
+    .from('types')
+    .where({
+        id: req.body.type_id
+    });
+
+    var newLeftToPay = req.body.left_to_pay - oldTypeAdditionalInfo[0].cost + newTypeAdditionalInfo[0].cost
+
     await database('subs').
     update({
         sub_number: req.body.sub_number,
         type_id: req.body.type_id,
         client_id: req.body.client_id,
         trainer_id: req.body.trainer_id,
-        left_to_pay: req.body.left_to_pay, 
+        left_to_pay: newLeftToPay, 
         begin_date: req.body.begin_date,
         end_date: req.body.end_date,
         training_left: req.body.training_left,
