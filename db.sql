832f2b9bf55026207f2be65ef972b52f37804ae3
Nikita Dunaev <priest9494@yandex.ru>
2020-05-11 11:09:25 +0700

    Added triggers for logging table changes

--- db.sql
+++ db.sql
@@ -60,6 +60,7 @@ CREATE TABLE visits (
     visit_time time
 );
 
+-- Ограничения на таблицы
 ALTER TABLE subs ADD CHECK (type_id > 0);
 ALTER TABLE subs ADD CHECK (client_id > 0);
 ALTER TABLE subs ADD CHECK (trainer_id > 0);
@@ -74,31 +75,164 @@ REFERENCES trainers (id) ON UPDATE NO ACTION ON DELETE SET NULL;
 ALTER TABLE subs ADD CONSTRAINT typefk FOREIGN KEY (type_id)
 REFERENCES types (id) ON UPDATE NO ACTION ON DELETE SET NULL;
 
+-- Триггер для протоколирования изменений  'types'
 CREATE TABLE types_audit (
     operation varchar(1) NOT NULL,
     stamp timestamp NOT NULL,
-    id SERIAL PRIMARY KEY,
+    id integer,
     title varchar(50),
     cost integer,
     training smallint
 );
 
-CREATE OR REPLACE FUNCTION process_types_audit() RETURNS TRIGGER AS $types_audit$
+CREATE FUNCTION process_types_audit() RETURNS TRIGGER AS $$
     BEGIN
         IF (TG_OP = 'DELETE') THEN
-            INSERT INTO emp_audit SELECT 'D', now(), OLD.*;
+            INSERT INTO types_audit SELECT 'D', now(), OLD.*;
             RETURN OLD;
         ELSIF (TG_OP = 'UPDATE') THEN
-            INSERT INTO emp_audit SELECT 'U', now(), NEW.*;
+            INSERT INTO types_audit SELECT 'U', now(), NEW.*;
             RETURN NEW;
         ELSIF (TG_OP = 'INSERT') THEN
-            INSERT INTO emp_audit SELECT 'I', now(), NEW.*;
+            INSERT INTO types_audit SELECT 'I', now(), NEW.*;
             RETURN NEW;
         END IF;
-        RETURN NULL; -- возвращаемое значение для триггера AFTER игнорируется
+        RETURN NULL;
     END;
-$emp_audit$ LANGUAGE plpgsql;
+$$LANGUAGE plpgsql;
 
 CREATE TRIGGER types_audit
 AFTER INSERT OR UPDATE OR DELETE ON types
     FOR EACH ROW EXECUTE PROCEDURE process_types_audit();
+
+-- Триггер для протоколирования изменений  'subs'
+CREATE TABLE subs_audit (
+    operation varchar(1) NOT NULL,
+    stamp timestamp NOT NULL,
+    id integer,
+    sub_number varchar(13),
+    sub_status boolean,
+    type_id integer,
+    client_id integer,
+    trainer_id integer,
+    left_to_pay integer,
+    begin_date date,
+    end_date date,
+    training_left smallint,
+    start_time time,
+    note varchar(200)
+);
+
+CREATE FUNCTION process_subs_audit() RETURNS TRIGGER AS $$
+    BEGIN
+        IF (TG_OP = 'DELETE') THEN
+            INSERT INTO subs_audit SELECT 'D', now(), OLD.*;
+            RETURN OLD;
+        ELSIF (TG_OP = 'UPDATE') THEN
+            INSERT INTO subs_audit SELECT 'U', now(), NEW.*;
+            RETURN NEW;
+        ELSIF (TG_OP = 'INSERT') THEN
+            INSERT INTO subs_audit SELECT 'I', now(), NEW.*;
+            RETURN NEW;
+        END IF;
+        RETURN NULL;
+    END;
+$$LANGUAGE plpgsql;
+
+CREATE TRIGGER subs_audit
+AFTER INSERT OR UPDATE OR DELETE ON subs
+    FOR EACH ROW EXECUTE PROCEDURE process_subs_audit();
+
+-- Триггер для протоколирования изменений  'clients'
+CREATE TABLE clients_audit (
+    operation varchar(1) NOT NULL,
+    stamp timestamp NOT NULL,
+    id integer,
+    fio varchar(100),
+    phone_number varchar(18),
+    first_visit_date date,
+    how_to_find varchar(16),
+    inviter_id varchar(18),
+    note varchar(200)
+);
+
+CREATE FUNCTION process_clients_audit() RETURNS TRIGGER AS $$
+    BEGIN
+        IF (TG_OP = 'DELETE') THEN
+            INSERT INTO clients_audit SELECT 'D', now(), OLD.*;
+            RETURN OLD;
+        ELSIF (TG_OP = 'UPDATE') THEN
+            INSERT INTO clients_audit SELECT 'U', now(), NEW.*;
+            RETURN NEW;
+        ELSIF (TG_OP = 'INSERT') THEN
+            INSERT INTO clients_audit SELECT 'I', now(), NEW.*;
+            RETURN NEW;
+        END IF;
+        RETURN NULL;
+    END;
+$$LANGUAGE plpgsql;
+
+CREATE TRIGGER clients_audit
+AFTER INSERT OR UPDATE OR DELETE ON clients
+    FOR EACH ROW EXECUTE PROCEDURE process_clients_audit();
+
+-- Триггер для протоколирования изменений  'payments'
+CREATE TABLE payments_audit (
+    operation varchar(1) NOT NULL,
+    stamp timestamp NOT NULL,
+    id integer,
+    sub_id integer,
+    payment_date date,
+    payment_amount integer,
+    payment_method varchar(3),
+    interest_rate integer
+);
+
+CREATE FUNCTION process_payments_audit() RETURNS TRIGGER AS $$
+    BEGIN
+        IF (TG_OP = 'DELETE') THEN
+            INSERT INTO payments_audit SELECT 'D', now(), OLD.*;
+            RETURN OLD;
+        ELSIF (TG_OP = 'UPDATE') THEN
+            INSERT INTO payments_audit SELECT 'U', now(), NEW.*;
+            RETURN NEW;
+        ELSIF (TG_OP = 'INSERT') THEN
+            INSERT INTO payments_audit SELECT 'I', now(), NEW.*;
+            RETURN NEW;
+        END IF;
+        RETURN NULL;
+    END;
+$$LANGUAGE plpgsql;
+
+CREATE TRIGGER payments_audit
+AFTER INSERT OR UPDATE OR DELETE ON payments
+    FOR EACH ROW EXECUTE PROCEDURE process_payments_audit();
+
+-- Триггер для протоколирования изменений  'trainers'
+CREATE TABLE trainers_audit (
+    operation varchar(1) NOT NULL,
+    stamp timestamp NOT NULL,
+    id integer,
+    fio varchar(100),
+    date_birth date
+);
+
+CREATE FUNCTION process_trainers_audit() RETURNS TRIGGER AS $$
+    BEGIN
+        IF (TG_OP = 'DELETE') THEN
+            INSERT INTO trainers_audit SELECT 'D', now(), OLD.*;
+            RETURN OLD;
+        ELSIF (TG_OP = 'UPDATE') THEN
+            INSERT INTO trainers_audit SELECT 'U', now(), NEW.*;
+            RETURN NEW;
+        ELSIF (TG_OP = 'INSERT') THEN
+            INSERT INTO trainers_audit SELECT 'I', now(), NEW.*;
+            RETURN NEW;
+        END IF;
+        RETURN NULL;
+    END;
+$$LANGUAGE plpgsql;
+
+CREATE TRIGGER trainers_audit
+AFTER INSERT OR UPDATE OR DELETE ON trainers
+    FOR EACH ROW EXECUTE PROCEDURE process_trainers_audit();
--- dbComplexQueries.sql
+++
@@ -1,9 +0,0 @@
-/* Client register: get client info */
-SELECT subs.begin_date, subs.end_date, subs.training_left, subs.left_to_pay, clients.fio, types.title, types.training, types.cost
-FROM subs, clients, types
-WHERE (subs.type_id = types.id AND subs.client_id = clients.id AND subs.sub_number = '1');
-
-/* Client register: mark visit */
-UPDATE subs
-SET training_left = training_left - 1
-WHERE subs.sub_number = '1';
